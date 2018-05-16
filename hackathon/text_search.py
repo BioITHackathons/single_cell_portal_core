61e13aff19106d7dca396982f6ac586d0ea24ff7
Asya  Lushnikova <asyalushnikova@gmail.com>
2018-05-16 11:01:39 -0400

    Update text_search.py
    ~ added a code for saving to a file a raw output concordance data

--- hackathon/text_search.py
+++ hackathon/text_search.py
@@ -8,12 +8,19 @@ Created on Mon May 14 08:10:43 2018
 from nltk import word_tokenize
 from nltk.text import Text 
 import json
+import sys
+from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import asksaveasfilename
 
 # import nltk
 #nltk.download() # download datasets
+ 
+in_file = askopenfilename(initialdir="/", title="Select file", filetypes=(("text files", "*.txt"), ("all files","*.*")))
+input_file = open(in_file, 'r')
 
-file = open('data_file.txt')
-data = file.read()
+#file = open('data_file.txt')
+
+data = input_file.read()
 
 tokens = word_tokenize(data)
 text = Text(tokens)
@@ -27,11 +34,18 @@ with open("diseases.json") as json_file:
 
     dict_tokens = word_tokenize(search_token)    
     dict_token = Text(dict_tokens)  
-    print(dict_token)
-     
+#print(dict_token)
+    
+out_file = asksaveasfilename(initialdir="/", title="Select file", filetypes=(("text files", "*.txt"), ("all files","*.*")))
+output_file = open(out_file, 'w')
+
+tmpout = sys.stdout
+sys.stdout = output_file
+ 
 for i in dict_token:    
-    concord = text.concordance(i) 
-    search = text.findall(r"<.*><.*><"+i+">")
-   
-file.close()
+        concord = text.concordance(i)   
+        search_token = text.findall(r"<.*><.*><"+i+">") 
+        
+input_file.close()
 json_file.close()
+output_file.close()
